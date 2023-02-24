f0568f8021a0d685258e85bfb6c223dbe3bd3d57
Johan Nenzén <johan.nanzen@gmail.com>
2023-02-24 17:22:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyflichub-tcpclient",
-    version="0.0.1",
+    version="0.1.0",
     author="JohNan",
     author_email="johan.nanzen@gmail.com",
     description="Asynchronous Python TCP Client for FlicHub",
