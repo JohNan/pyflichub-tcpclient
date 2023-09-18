f35d4dcab2d0b39af3acf8467be81ec57ab854f7
Johan Nenzén <johan.nanzen@gmail.com>
2023-09-19 00:08:31 +0200

    Adds missing file

---
+++ pyflichub/network.py
@@ -0,0 +1,56 @@
+from typing import List
+
+
+class WifiInfo:
+    state: str
+    ssid: str
+
+    def __init__(self, connected, ip, mac):
+        self.connected = connected
+        self.ip = ip
+        self.mac = mac
+
+
+class EthernetInfo:
+    def __init__(self, connected, ip, mac):
+        self.connected = connected
+        self.ip = ip
+        self.mac = mac
+
+
+class DhcpInfo:
+    def __init__(self, wifi=None, ethernet=None):
+        self.wifi = WifiInfo(**wifi) if wifi else None
+        self.ethernet = EthernetInfo(**ethernet) if ethernet else None
+
+
+class _WifiState:
+    def __init__(self, state, ssid):
+        self.state = state
+        self.ssid = ssid
+
+
+def _decode_ssid(ssid_list):
+    return ''.join(chr(byte) for byte in ssid_list)
+
+
+class Network:
+    def __init__(self, dhcp, wifi_state=None):
+        self._dhcp = DhcpInfo(**dhcp)
+        self._dhcp.wifi.state = wifi_state.get('state', None) if wifi_state else None
+        self._dhcp.wifi.ssid = _decode_ssid(wifi_state.get('ssid', None)) if wifi_state else None
+
+    def has_wifi(self) -> bool:
+        return self._dhcp.wifi is not None
+
+    def has_ethernet(self) -> bool:
+        return self._dhcp.ethernet is not None
+
+    @property
+    def wifi(self) -> WifiInfo:
+        return self._dhcp.wifi
+
+    @property
+    def ethernet(self) -> EthernetInfo:
+        return self._dhcp.ethernet
+
