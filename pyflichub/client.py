db4f4e07e599ab54b12072505f4d43f33ad6e74a
martin <mg@macgenius.me>
2024-06-19 20:26:12 +0200

    add buffer for too long packets

--- pyflichub/client.py
+++ pyflichub/client.py
@@ -48,6 +48,7 @@ class FlicHubTcpClient(asyncio.Protocol):
         self._reconnect_timeout = reconnect_timeout
         self._timeout = timeout
         self._data: dict = {}
+        self._buffer = None
         self._connecting = False
         self._forced_disconnect = False
         self.async_on_connected = None
@@ -139,6 +140,18 @@ class FlicHubTcpClient(asyncio.Protocol):
 
     def data_received(self, data):
         decoded_data = data.decode()
+        if "\n" not in decoded_data:
+            if not self._buffer:
+                _LOGGER.debug('First data fragment received: {!r}'.format(decoded_data))
+                self._buffer = decoded_data
+            else:
+                _LOGGER.debug('Data fragment received: {!r}'.format(decoded_data))
+                self._buffer += decoded_data
+            return
+        else:
+            if self._buffer:
+                decoded_data = self._buffer + decoded_data
+               
         _LOGGER.debug('Data received: {!r}'.format(decoded_data))
         for data_part in [data_part for data_part in decoded_data.split("\n") if data_part.strip()]:
             if data_part == 'pong':
@@ -153,6 +166,8 @@ class FlicHubTcpClient(asyncio.Protocol):
             except Exception as e:
                 _LOGGER.warning(e, exc_info=True)
                 _LOGGER.warning('Unable to decode received data')
+
+        self._buffer = None
 
     def connection_lost(self, exc):
         _LOGGER.info("Connection lost")
